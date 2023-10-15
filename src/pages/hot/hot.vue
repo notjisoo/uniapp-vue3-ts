c8d46ff20f52ed48876bd76873f3b04bb0d0940a
清远彭于晏之分晏 <10614847+sifeiaa@user.noreply.gitee.com>
2023-10-15 18:07:11 +0800

    热门推荐数据获取

--- src/pages/hot/hot.vue
+++ src/pages/hot/hot.vue
@@ -1,5 +1,8 @@
-// /src/pages/hot/hot.vue
+<!-- // /src/pages/hot/hot.vue -->
 <script setup lang="ts">
+import { getHotRecommendAPI } from "@/services/hot";
+import { onLoad } from "@dcloudio/uni-app";
+
 // 热门推荐页 标题和url
 const hotMap = [
   { type: "1", title: "特惠推荐", url: "/hot/preference" },
@@ -13,7 +16,19 @@ const query = defineProps<{ type: string }>();
 const currUrlMap = hotMap.find((v) => v.type === query.type);
 // 动态设置标题
 uni.setNavigationBarTitle({
+  // 利用非空断言排除掉undefined的情况
   title: currUrlMap!.title,
+});
+
+// 获取热门推荐数据
+const getHotRecommendData = async () => {
+  const res = await getHotRecommendAPI(currUrlMap!.url);
+  console.log(res);
+};
+
+// 页面加载
+onLoad(() => {
+  getHotRecommendData();
 });
 </script>
 
---
+++ src/services/hot.ts
@@ -0,0 +1,18 @@
+import type { PageParams } from "@/types/global";
+import { http } from "@/utils/http";
+
+// 利用&, 去给PageParams添加多一个参数为subType类型为string
+type hotParams = PageParams & { subType?: string };
+/**
+ * 通用热门推荐类型
+ * @param url 请求地址
+ * @param data 请求参数
+ * @returns
+ */
+export const getHotRecommendAPI = (url: string, data?: hotParams) => {
+  return http({
+    method: "GET",
+    url,
+    data,
+  });
+};
