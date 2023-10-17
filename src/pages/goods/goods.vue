a1d844429c7c8f729f251a3b4c721b2f47c92f6e
清远彭于晏之分晏 <10614847+sifeiaa@user.noreply.gitee.com>
2023-10-17 17:32:19 +0800

    商品弹出层的交互，add address + service

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -4,9 +4,26 @@ import { getGoodsAPI } from "@/services/goods";
 import type { GoodsResult } from "@/types/goods";
 import { onLoad } from "@dcloudio/uni-app";
 import { ref } from "vue";
+import AddressPanel from "./components/AddressPanel.vue";
+import ServicePanel from "./components/ServicePanel.vue";
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync();
+
+// uni-ui 弹出层组件 ref
+const popup = ref<{
+  open: (type?: UniHelper.UniPopupType) => void;
+  close: () => void;
+}>();
+// 弹出层条件渲染
+const popupName = ref<"address" | "service">();
+
+const openPopup = (name: typeof popupName.value) => {
+  // 修改弹出层名称
+  popupName.value = name;
+  popup.value?.open();
+};
+
 // 接收页面传递过来的id，通过ID来渲染数据
 const query = defineProps<{ id: string }>();
 
@@ -75,11 +92,11 @@ onLoad(() => {
           <text class="label">选择</text>
           <text class="text ellipsis"> 请选择商品规格 </text>
         </view>
-        <view class="item arrow">
+        <view @tap="openPopup('address')" class="item arrow">
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view class="item arrow">
+        <view @tap="openPopup('service')" class="item arrow">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -161,6 +178,17 @@ onLoad(() => {
       <view class="buynow"> 立即购买 </view>
     </view>
   </view>
+
+  <!-- uni-ui 弹出层 -->
+  <uni-popup
+    ref="popup"
+    type="bottom"
+    background-color="
+  #fff"
+  >
+    <AddressPanel v-if="popupName === 'address'" @close="popup?.close()" />
+    <ServicePanel v-if="popupName === 'service'" @close="popup?.close()" />
+  </uni-popup>
 </template>
 
 <style lang="scss">
